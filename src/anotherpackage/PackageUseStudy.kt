1c2fe9968b1d9ecbb39375440fe57cb0d7b00c36
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-07-03 21:21:22 +0900

    210703

--- src/anotherpackage/PackageUseStudy.kt
+++ src/anotherpackage/PackageUseStudy.kt
@@ -1,14 +1,14 @@
-package anotherpackage
-
-import mypackage.MY_CONSTANT
-import mypackage.sayHello
-import mypackage.MyClass
-
-fun main(args: Array<String>) {
-
-    println(MY_CONSTANT)
-    sayHello("김철수")
-    var c = MyClass()
-    c.print()
-
-}
+//package anotherpackage
+//
+//import mypackage.MY_CONSTANT
+//import mypackage.sayHello
+//import mypackage.MyClass
+//
+//fun main(args: Array<String>) {
+//
+//    println(MY_CONSTANT)
+//    sayHello("김철수")
+//    var c = MyClass()
+//    c.print()
+//
+//}
